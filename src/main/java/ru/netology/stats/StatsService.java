ddb396de25ecf586e674f46730536e78dd5b436e
Soboleva Ekaterina <soboleva.ketti0403@gmail.com>
2023-01-11 19:28:23 +0300

    1

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,65 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale / 12;
+    }
+
+    public int maximum(long[] sales) {
+        int maximumMonth = 0;
+        long maximum = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (maximum <= sales[i]) {
+                maximum = sales[i];
+                maximumMonth = i;
+            }
+        }
+        return maximumMonth;
+    }
+
+    public int minimum(long[] sales) {
+        int minimumMonth = 0;
+        long minimum = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (minimum >= sales[i]) {
+                minimum = sales[i];
+                minimumMonth = i;
+            }
+        }
+        return minimumMonth;
+    }
+    public int monthLowerAverage(long[] sales) {
+        int counter = 0;
+        long average = average(sales);
+        for (long sale: sales) {
+            if (sale < average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int monthAboveAverage(long[] sales) {
+        int counter = 0;
+        long average = average(sales);
+        for (long sale: sales) {
+            if (sale > average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+}
